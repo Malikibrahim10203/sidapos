4ab44db449857a4020491199b38a20c8a51ef9b4
malikibrahim10203 <malikibr4h1@gmail.com>
2023-05-13 09:53:39 +0700

    sql

--- sidapos.sql
+++ sidapos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2023 at 01:48 AM
+-- Generation Time: May 13, 2023 at 04:52 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -24,31 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `adminpokja`
+-- Table structure for table `balita`
 --
 
-CREATE TABLE `adminpokja` (
-  `idadmin` int(11) NOT NULL,
+CREATE TABLE `balita` (
+  `idbalita` int(11) NOT NULL,
   `namalengkap` varchar(25) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `alamat` text NOT NULL,
-  `jabatan` varchar(25) NOT NULL
+  `alamat` varchar(25) NOT NULL,
+  `umur` int(11) NOT NULL,
+  `statusImunisasi` varchar(25) NOT NULL,
+  `idposyandu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `balita`
+-- Table structure for table `failed_jobs`
 --
 
-CREATE TABLE `balita` (
-  `Idbalita` int(11) NOT NULL,
-  `namalengkap` varchar(25) NOT NULL,
-  `alamat` varchar(25) NOT NULL,
-  `umur` int(11) NOT NULL,
-  `statusImunisasi` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -60,23 +62,62 @@ CREATE TABLE `ibuhamil` (
   `idibuhamil` int(11) NOT NULL,
   `namalengkap` varchar(25) NOT NULL,
   `alamat` varchar(25) NOT NULL,
-  `umurkehamilan` int(11) NOT NULL
+  `umurkehamilan` int(11) NOT NULL,
+  `idposyandu` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kader`
+-- Table structure for table `migrations`
 --
 
-CREATE TABLE `kader` (
-  `idkader` int(11) NOT NULL,
-  `namalengkap` varchar(25) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `idposyandu` int(11) NOT NULL,
-  `jabatan` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -86,40 +127,91 @@ CREATE TABLE `kader` (
 
 CREATE TABLE `posyandu` (
   `idposyandu` int(11) NOT NULL,
-  `jumlahkader` int(11) NOT NULL,
   `pos` varchar(25) NOT NULL,
   `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posyandu`
+--
+
+INSERT INTO `posyandu` (`idposyandu`, `pos`, `alamat`) VALUES
+(1, 'Anggrek', 'Jl.bojongSoang\r\n'),
+(2, 'Mawar', 'Jl.Baleendah'),
+(3, 'balai desa', 'jl.Dago');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `namalengkap` varchar(25) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `idposyandu` int(11) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `namalengkap`, `username`, `email`, `email_verified_at`, `password`, `jabatan`, `idposyandu`, `remember_token`, `created_at`, `updated_at`) VALUES
+(9, 'anikader', 'anikader', 'adnikader@example.com', NULL, '$2y$10$y57ttE3LxDmaHTmAYi8RyeqU7BmkA.dsDVW5lBAhcwFCTCCE.gwim', 'kader', 1, NULL, '2023-05-12 01:32:49', '2023-05-12 01:32:49'),
+(10, 'susan kader', 'susan', 'susan@example.com', NULL, '$2y$10$YJFQu8F8JatWiiYwVFWFoOp.qfKETHSuWQkTzdcyrvlX6arL06QiW', 'admin', 1, NULL, '2023-05-12 01:38:25', '2023-05-12 01:38:25');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `adminpokja`
---
-ALTER TABLE `adminpokja`
-  ADD PRIMARY KEY (`idadmin`);
-
---
 -- Indexes for table `balita`
 --
 ALTER TABLE `balita`
-  ADD PRIMARY KEY (`Idbalita`);
+  ADD PRIMARY KEY (`idbalita`),
+  ADD KEY `idposyandu` (`idposyandu`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
 -- Indexes for table `ibuhamil`
 --
 ALTER TABLE `ibuhamil`
-  ADD PRIMARY KEY (`idibuhamil`);
+  ADD PRIMARY KEY (`idibuhamil`),
+  ADD KEY `idposyandu` (`idposyandu`);
 
 --
--- Indexes for table `kader`
+-- Indexes for table `migrations`
 --
-ALTER TABLE `kader`
-  ADD PRIMARY KEY (`idkader`),
-  ADD KEY `pos` (`idposyandu`),
-  ADD KEY `idposyandu` (`idposyandu`);
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
 -- Indexes for table `posyandu`
@@ -128,14 +220,68 @@ ALTER TABLE `posyandu`
   ADD PRIMARY KEY (`idposyandu`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `idposyandu` (`idposyandu`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posyandu`
+--
+ALTER TABLE `posyandu`
+  MODIFY `idposyandu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `kader`
+-- Constraints for table `balita`
 --
-ALTER TABLE `kader`
-  ADD CONSTRAINT `kader_ibfk_1` FOREIGN KEY (`idposyandu`) REFERENCES `posyandu` (`idposyandu`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `balita`
+  ADD CONSTRAINT `balita_ibfk_1` FOREIGN KEY (`idposyandu`) REFERENCES `posyandu` (`idposyandu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ibuhamil`
+--
+ALTER TABLE `ibuhamil`
+  ADD CONSTRAINT `ibuhamil_ibfk_1` FOREIGN KEY (`idposyandu`) REFERENCES `posyandu` (`idposyandu`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`idposyandu`) REFERENCES `posyandu` (`idposyandu`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
