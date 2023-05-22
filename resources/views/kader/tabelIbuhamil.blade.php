67798fbbba584ad836de8ef22cdf2b53f7539c7f
malikibrahim10203 <malikibr4h1@gmail.com>
2023-05-23 01:17:49 +0700

    new repo Malik

--- resources/views/kader/tabelIbuhamil.blade.php
+++ resources/views/kader/tabelIbuhamil.blade.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{asset('css/tambahIbuhamil.css')}}">
+    <link rel="stylesheet" href="{{asset('css/tabelIbuhamil.css')}}">
     <script src="{{asset('js/dashboardAdmin.js')}}"></script>
     <title>Dashboard</title>
 </head>
 <body>
+
     <div class="sidebar p-4" id="sidebar">
-        <h3 class="mb-5 text-black">Sidapos</h3>
+        <h3 class="mb-5 text-black"><a href="dashboardadmin">Sidapos</a></h3>
         <h5 class="mt-5">Menu</h5>
         <li>
             <div class="dropdown">
@@ -46,7 +48,7 @@
     <section class="p-4" id="nav-content">
         
         <nav class="navbar navbar-expand-lg">
-            <div class="container">
+        <div class="container">
                 <button class="btn btn-light" id="button-toggle">
                     <i class="bi bi-three-dots"></i>
                 </button>
